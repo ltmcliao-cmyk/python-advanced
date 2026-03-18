84f6037c96e13c15ead1ab46ed3a5340153f7333
Hao-Chun Liao <ltmc.liao@gmail.com>
2026-03-18 15:21:38 +0800

    0318

--- 1/PYD101.py
+++ 1/PYD101.py
@@ -1,7 +1,14 @@
 #TODO
-
+#TODO
+a = eval(input())
+b = eval(input())
+c = eval(input())
+d = eval(input())
 #向右靠齊
 #TODO
-
+print(f'|{a:>5} {b:>5}|') 
+print(f'|{c:>5} {d:>5}|')
 #向左靠齊
 #TODO
+print(f'|{a:<5} {b:<5}|')
+print(f'|{c:<5} {d:<5}|')
--- 1/PYD102.py
+++ 1/PYD102.py
@@ -1,7 +1,13 @@
 # TODO
-
+a = eval(input())
+b = eval(input())
+c = eval(input())
+d = eval(input())
 # 靠右對齊
 # TODO
-
+print(f"|{a} {b}|")
+print(f"|{c} {d}|")
 # 靠左對齊
 # TODO
+print(f"|{a} {b}|")
+print(f"|{c} {d}|")
